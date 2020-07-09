17793e391626bee568ebde1e4096495f1dbf8a8c
dinnawin <dinnawin@gmail.com>
2020-07-09 17:26:46 +0800

    Update tranfser_json_from_influxdb_to_txt.py

--- tranfser_json_from_influxdb_to_txt.py
+++ tranfser_json_from_influxdb_to_txt.py
@@ -10,6 +10,6 @@ with open("/tmp/all_data_json.txt","r") as f1:
 with open("/tmp/all_data_plain.txt","w") as f2:
     for i in m["results"][0]["series"][0]["values"]:
         t = "domain: {} request_url: {} status_code: {}".format(i[1].ljust(25,' '),
-                                                                  i[2].ljust(60,' '),
-                                                                  i[3].ljust(10,' ') ) 
+                                                                i[2].ljust(60,' '),
+                                                                i[3].ljust(10,' ') ) 
         f2.write(t + '\n')
